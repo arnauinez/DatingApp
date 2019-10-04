e774cbaefaa0e82ce5d4c1550f8ece7e7e8d8604
Martinez Arnau <arnau27hk@gmail.com>
2019-10-04 13:06:30 +0200

    Initial Commit

---
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.css']
+})
+export class ValueComponent implements OnInit {
+  values: any; // Just any javascript variable
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getValues();
+  }
+
+  getValues() {
+    this.http
+    // Deberia tiparse siempre que se pueda ----> get<string[]>('http://localhost:5000/api/values')
+      .get<string[]>('http://localhost:5000/api/values')
+      .subscribe(response => {
+        this.values = response;
+      }, error => {
+        console.log(error);
+      });
+  }
+
+  // handleResponse = (response: string[]) => {
+  //   this.values = response;
+  // }
+
+  // handleError = (error: HttpErrorResponse) => {
+  //   console.log(error);
+  // }
+
+  testIIFE = () => {
+    (() => {
+      const kiwi = 0;
+      console.log(kiwi);
+    })();
+    (() => {
+      const kiwi = 9;
+      console.log(kiwi);
+    })();
+  }
+}
---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    // http://localhost:5000/api/values
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues() //Allows us to return HTTP responses like 200 OK
+        {
+            var values = await _context.Values.ToListAsync(); //Storing values into a list and returning them asynchronously 
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
+        {
+            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
